96d906450473ed8189acc4d25bcea00b14ea552a
SojerSM <seb.maz1996@gmail.com>
2023-04-28 15:14:16 +0200

    [UPDATE] V0.2.1
    Given .txt file is now checked for requirements, if any line of an input does not meet those, klops is returned

--- src/main/java/Bricks.java
+++ src/main/java/Bricks.java
@@ -14,13 +14,18 @@ import java.util.Scanner;
  * @since 2023-04-27
  */
 public class Bricks {
+    private static final String BAD_INPUT_MESSAGE = "klops";
     private static final Scanner scanner = new Scanner(System.in);
     private static final Generator generator = new Generator();
 
     public static void main(String[] args) {
-
 //        generateBlocksList(20);
-        Box box = new Box(readFileFromMethodArg());
+        Box box = new Box(readFileFromMethodArg(args));
+
+        if (box.getBlocks() == null) {
+            System.out.println(BAD_INPUT_MESSAGE);
+            return;
+        }
 
         for (Map.Entry<Integer, String[]> entry : box.getBlocks().entrySet()) {
             Integer key = entry.getKey();
@@ -28,7 +33,6 @@ public class Bricks {
 
             System.out.println(key + ": " + values[0] + " " + values[1]);
         }
-
     }
 
     public static void generateBlocksList(int listSize) {
@@ -44,17 +48,52 @@ public class Bricks {
      *
      * @return a hashmap.
      */
-    public static Map<Integer, String[]> readFileFromMethodArg() {
+    private static Map<Integer, String[]> readFileFromMethodArg(String[] args) {
         Map<Integer, String[]> transformed = new HashMap<>();
         int i = 0;
 
         while(scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] parts = line.split(":");
+
+            // if given input doesn't meet requirements break the loop and return null;
+            if (!validateInput(parts)) return null;
+
             transformed.put(i,parts);
             i++;
         }
         scanner.close();
         return transformed;
+    }
+
+    /**
+     * This method checks conditions for given input.
+     *
+     * @param parts stands for blueprint index which should be a positive natural number
+     *              and block code which should be all uppercase and exactly 4 characters
+     *              long.
+     * @return true if all conditions are meet, false if input is wrong in any way.
+     */
+    private static boolean validateInput(String[] parts) {
+        if (parts[1].length() != 4) return false;
+
+        if (!isUpperCase(parts[1])) return false;
+
+        try {
+            Integer.parseInt(parts[0]);
+        } catch(NumberFormatException err) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isUpperCase(String code) {
+        char[] codeArr = code.toCharArray();
+
+        for (int i = 0; i < codeArr.length; i++) {
+            if (!Character.isUpperCase(codeArr[i])) return false;
+        }
+        return true;
     }
 }
